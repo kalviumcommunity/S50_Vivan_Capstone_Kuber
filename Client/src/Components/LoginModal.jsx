684256b59ce9fec1fd56c09dc88b346517694575
vivan raj <vivanrajmittakodi6@gmail.com>
2024-05-18 20:32:40 +0530

    update

--- Client/src/Components/LoginModal.jsx
+++ Client/src/Components/LoginModal.jsx
@@ -11,6 +11,11 @@ function LoginModal({ onClose }) {
     setShowEmailLogin(true);
   };
 
+  const handleGoogleSignUp = () => {
+    window.location.href = "http://localhost:3000/auth/google";
+  };
+
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
     <div className="bg-slate-200 w-1/4 p-5 rounded-lg">
@@ -20,7 +25,7 @@ function LoginModal({ onClose }) {
       <h2 className="text-xl mb-2">Login to Kuber</h2>
       {!showEmailLogin ? (
         <div className="space-y-4">
-          <button className="flex justify-evenly items-center gap-2 w-full py-2 rounded-lg border-2 border-slate-500 text-base font-medium text-gray-700 bg-white">
+          <button className="flex justify-evenly items-center gap-2 w-full py-2 rounded-lg border-2 border-slate-500 text-base font-medium text-gray-700 bg-white" onClick={handleGoogleSignUp}>
             <img className="h-6 w-8" src={google} alt="Google" />
             Login with Google
           </button>
